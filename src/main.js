918bbdb8239786332acdf88c8f1e1c8d278bb7b4
Shota Matsuda <shota@takram.com>
2018-04-18 12:35:43 +0900

    bundle and transpile the module target

---
+++ src/main.js
@@ -0,0 +1,62 @@
+// The MIT License
+// Copyright (C) 2016-Present Shota Matsuda
+
+import Binder from './Binder'
+import Binding from './Binding'
+import CustomEvent from './CustomEvent'
+import Event, { modifyEvent } from './Event'
+import EventBundle from './EventBundle'
+import EventDispatcher from './EventDispatcher'
+import EventDispatcherMixin from './EventDispatcherMixin'
+import EventTarget from './EventTarget'
+import EventTargetMixin from './EventTargetMixin'
+import GenericEvent from './GenericEvent'
+import KeyboardEvent from './KeyboardEvent'
+import MouseEvent from './MouseEvent'
+import StateEvent from './StateEvent'
+import Touch from './Touch'
+import TouchEvent from './TouchEvent'
+import TouchList from './TouchList'
+import WheelEvent from './WheelEvent'
+
+export {
+  Binder,
+  Binding,
+  CustomEvent,
+  Event,
+  EventBundle,
+  EventDispatcher,
+  EventDispatcherMixin,
+  EventTarget,
+  EventTargetMixin,
+  GenericEvent,
+  KeyboardEvent,
+  modifyEvent,
+  MouseEvent,
+  StateEvent,
+  Touch,
+  TouchEvent,
+  TouchList,
+  WheelEvent
+}
+
+export default {
+  Binder,
+  Binding,
+  CustomEvent,
+  Event,
+  EventBundle,
+  EventDispatcher,
+  EventDispatcherMixin,
+  EventTarget,
+  EventTargetMixin,
+  GenericEvent,
+  KeyboardEvent,
+  modifyEvent,
+  MouseEvent,
+  StateEvent,
+  Touch,
+  TouchEvent,
+  TouchList,
+  WheelEvent
+}
