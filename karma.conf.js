d7b59296137f787ff1a61d89053ec94d95de5ec0
Shota Matsuda <shota@takram.com>
2018-08-02 23:43:49 +0900

    Add karma for test runner

---
+++ karma.conf.js
@@ -0,0 +1,43 @@
+// The MIT License
+// Copyright (C) 2017-Present Shota Matsuda
+
+const webpack = require('./webpack.config.test.js')
+
+module.exports = function (config) {
+  config.set({
+    basePath: './',
+    plugins: [
+      require('karma-chrome-launcher'),
+      require('karma-coverage-istanbul-reporter'),
+      require('karma-mocha'),
+      require('karma-sourcemap-loader'),
+      require('karma-spec-reporter'),
+      require('karma-webpack')
+    ],
+    frameworks: ['mocha'],
+    files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'node_modules/mocha/mocha.js',
+      'node_modules/chai/chai.js',
+      'node_modules/sinon/pkg/sinon.js',
+      'test/src/**/*.js'
+    ],
+    preprocessors: {
+      'test/src/**/*.js': ['webpack', 'sourcemap']
+    },
+    reporters: ['spec', 'coverage-istanbul'],
+    proxies: {
+      '/node_modules/': '/base/node_modules/'
+    },
+    coverageIstanbulReporter: {
+      reports: ['text', 'text-summary', 'lcov'],
+      dir: 'coverage/browser',
+      combineBrowserReports: true
+    },
+    webpack,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    autoWatch: false,
+    concurrency: Infinity
+  })
+}
